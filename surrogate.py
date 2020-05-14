1e097c9a6166f84a15ffb929eeef11fb18b0007e
mishapadidar <mishapadidar14@gmail.com>
2020-05-14 15:05:29 -0400

    added CVaR to surrogates

--- surrogate.py
+++ surrogate.py
@@ -2,10 +2,10 @@
 # gaussian kernel
 
 import numpy as np
-from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.gaussian_process.kernels import WhiteKernel, ConstantKernel, RBF
 from sklearn.gaussian_process import GaussianProcessRegressor as GPR
 from scipy.optimize import minimize
+import cvxpy as cp
 
 
 class GaussianProcessRegressor():
@@ -110,3 +110,75 @@ class GaussianProcessRiskNeutral():
     self.X  = np.vstack((self.X,xx))
     self.fX = np.concatenate((self.fX,[yy]))
     self.fit(self.X, self.fX)
+
+
+
+
+class CVaR():
+  """CVaR/VaR surrogate
+  p: function handle for distribution
+  p takes in an integer number of points and returns a 2D array
+  of vectors
+  beta: confidence level for CVaR/VaR
+  num_points_MC: number of points used in Monte Carlo
+  """
+  
+  def __init__(self,kernel,p,beta = 0.95, num_points_MC = 500):
+    self.dim    = 0;    # input data dimension
+    self.X      = None; # data points
+    self.fX     = None; # function evals
+    self.GP     = GPR(kernel=kernel) # gaussian process
+    self.p      = p     # pdf for U
+    self.beta   = beta  # confidence level for CVaR
+    self.num_points_MC = num_points_MC # number of points for monte carlo
+
+
+  # "fit" a GP to the data
+  def fit(self, X,fX):
+    # update data
+    self.X  = X;
+    self.fX = fX;
+    self.dim = X.shape[1]
+
+    # fit sklearn GP
+    self.GP.fit(X,fX)
+
+
+  def predict(self, xx, std = False):
+    """predict Ghat(x) = min_alpha G_beta(x,alpha)
+    xx: 2D array of points
+    std: Bool
+    """
+    if std == True:
+      print('')
+      print("ERROR: CVaR has no variance")
+      quit()
+
+    # storage
+    N    = np.shape(xx)[0]
+    Ghat = np.zeros(N) 
+
+    # for each x in xx calculate Ghat(x)
+    for i in range(N):
+      # f(x-U) with Monte Carlo
+      U    = self.p(self.num_points_MC)
+      A    = self.GP.predict(xx[i]-U)
+
+      # solve the optimization problem
+      alpha = cp.Variable()
+      # average the positive values
+      cost = alpha + cp.sum(cp.pos(A-alpha))/(self.num_points_MC*(1.0-self.beta))
+      prob = cp.Problem(cp.Minimize(cost))
+      prob.solve()
+      Ghat[i] = prob.value
+
+    return Ghat
+
+
+
+  def update(self, xx,yy):
+    """  update gp with new points
+    """
+    self.X = np.vstack((self.X,xx))
+    self.fX = np.concatenate((self.fX,[yy]))
+    self.fit(self.X,self.fX)
